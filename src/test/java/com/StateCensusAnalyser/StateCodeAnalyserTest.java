0dcb592beda6a3d7167e788d86d1f004bc389a1c
Soumikpal9 <soumikpal1997@gmail.com>
2020-10-20 13:19:13 +0530

    [SP]Added: Test To Ensure wrong File Path

--- src/test/java/com/StateCensusAnalyser/StateCodeAnalyserTest.java
+++ src/test/java/com/StateCensusAnalyser/StateCodeAnalyserTest.java
@@ -8,6 +8,7 @@ import junit.framework.Assert;
 
 public class StateCodeAnalyserTest {
 	public static final String STATE_CODE_DATA = "StateCode.csv";
+	public static final String WRONG_STATE_CODE_DATA = "src/main/java/com/StateCensusAnalyser/StateCode.csv";
 	
 	StateCodeAnalyser stateCodeAnalyser = new StateCodeAnalyser();
 	
@@ -15,5 +16,15 @@ public class StateCodeAnalyserTest {
     public void ensureNoOfRecordMatches() throws CensusException{
         int records = stateCodeAnalyser.loadCSVFile(Paths.get(STATE_CODE_DATA));
         Assert.assertEquals(36, records);
+    }
+	
+	@Test
+    public void checkWrongPath() throws CensusException{
+    	try {
+    		stateCodeAnalyser.loadCSVFile(Paths.get(WRONG_STATE_CODE_DATA));
+    	}
+    	catch(CensusException e) {
+    		Assert.assertEquals(CensusException.ExceptionType.WRONG_CSV, e.type);
+    	}
     }
 }
