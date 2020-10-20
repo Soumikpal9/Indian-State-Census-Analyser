f967775f0f9aa16390914d23dd7ce7fffe209a4c
Soumikpal9 <soumikpal1997@gmail.com>
2020-10-20 11:57:28 +0530

    [SP]Added: Welcome Message

---
+++ src/main/java/com/StateCensusAnalyser/StateCensusAnalyser.java
@@ -0,0 +1,20 @@
+package com.StateCensusAnalyser;
+
+import java.awt.List;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class StateCensusAnalyser {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Indian State Census Analyser Problem!!!");
+	}
+}
---
+++ src/test/java/com/StateCensusAnalyser/StateCensusAnalyserTest.java
@@ -0,0 +1,13 @@
+package com.StateCensusAnalyser;
+
+import static org.junit.Assert.assertTrue;
+
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class StateCensusAnalyserTest {
+	
+}
